6a89f06ec44a6799da2d277d24688fcf5b232cae
igarashikoudainoMacBook-Pro <kodai.igarashi.2r@stu.hosei.ac.jp>
2021-10-13 01:12:03 +0900

    ありがとうございました感謝です、、、、、

--- 04_ゼミ/session/main.js
+++ 04_ゼミ/session/main.js
@@ -5,10 +5,11 @@ const memocontainer = document.getElementById("memo-container")
 button.onclick = function() {
   const card = document.createElement("div")
   card.className = "card"
+
   card.textContent = memoinput.value
   memocontainer.append(card)
-
   memoinput.value = ""
 }
 //const container = document.createElement("div")
 //container.className = "container"
+//memocontainer.append(container)
